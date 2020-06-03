3c215aa22423577e8321a722300a0ce60cef91b9
ApChris <achris.info@gmail.com>
2020-06-03 21:07:01 +0300

    initial commit

---
+++ whoServer/src/server.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+
+long queryPortNum;
+long statisticsPortNum;
+long numThreads;
+long bufferSize;
+
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 9)                          // Check if we have !=9 arguments
+    {
+      printf("ERROR INPUT!!\nGive for example : ./whoServer -q 100 -s 200 -w 5 -b 100\n");
+      return -1;
+    }
+
+    for(long i = 1; i < argc; i++)      // Get arguments
+    {
+      if(!strcmp(argv[i],"-q"))         // Get queryPortNum
+      {
+          queryPortNum = atol(argv[i+1]);
+      }
+      else if(!strcmp(argv[i],"-s"))    // Get statisticsPortNum
+      {
+         statisticsPortNum = atol(argv[i+1]);
+      }
+      else if(!strcmp(argv[i],"-w"))    // Get numThreads
+      {
+         numThreads = atol(argv[i+1]);
+      }
+      else if(!strcmp(argv[i],"-b"))    // Get buffer Size
+      {
+         bufferSize = atol(argv[i+1]);
+      }
+
+    }
+
+    return 0;
+}
