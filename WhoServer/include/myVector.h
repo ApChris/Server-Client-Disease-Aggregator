e0348ca9de13991a9e05c8e85d01033d6d339551
ApChris <achris.info@gmail.com>
2020-06-04 03:37:48 +0300

    myVector.h init

---
+++ WhoServer/include/myVector.h
@@ -0,0 +1,24 @@
+#ifndef __MYVECTOR_H__
+#define __MYVECTOR_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SIZE 20
+
+typedef struct myVector
+{
+    long length;
+    long size;
+    void ** items;
+}myVector;
+
+myVector * Init_MyVector();
+void PushBack_MyVector(myVector * vec, void * item);
+long Size_MyVector(myVector * vec);
+long Length_MyVector(myVector * vec);
+void * GetItem_MyVector(myVector * vec, long position);
+void UpdateItem_MyVector(myVector * vec, void * newItem, long position);
+void Deallocate_MyVector(myVector * vec);
+
+#endif
