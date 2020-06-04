cf895ac2f2d04ec5651c27a81c7565663225393b
ApChris <achris.info@gmail.com>
2020-06-04 03:38:12 +0300

    initial commit myVector.c

---
+++ WhoServer/src/myVector.c
@@ -0,0 +1,67 @@
+#include "../include/myVector.h"
+
+
+myVector * Init_MyVector()
+{
+    myVector * vec = (myVector *)malloc(sizeof(myVector));
+    vec -> length = 0;
+    vec -> items = (void **)malloc(sizeof(void *)* SIZE);
+    vec -> size = SIZE;
+
+    return vec;
+}
+
+long Size_MyVector(myVector * vec)
+{
+    return vec -> size;
+}
+long Length_MyVector(myVector * vec)
+{
+    return vec -> length;
+}
+
+
+void PushBack_MyVector(myVector * vec, void * item)
+{
+    if(Length_MyVector(vec) == Size_MyVector(vec))
+    {
+        vec -> size *= 2;
+        vec -> items = realloc(vec -> items, sizeof(void *) * vec -> size);
+    }
+
+    vec -> items[vec -> length] = item;
+    vec -> length++;
+}
+
+
+void * GetItem_MyVector(myVector * vec, long position)
+{
+    // Check is position is out of bounds
+    if(Length_MyVector(vec) > position)
+    {
+        return vec -> items[position];
+    }
+    else
+    {
+        return NULL;
+    }
+
+}
+
+void UpdateItem_MyVector(myVector * vec, void * newItem, long position)
+{
+    // Check is position is out of bounds
+    if(Length_MyVector(vec) > position)
+    {
+        // update item's value with new item
+        vec -> items[position] = newItem;
+        printf("%s\n",(char *)vec -> items[position]);
+    }
+
+}
+
+void Deallocate_MyVector(myVector * vec)
+{
+    free(vec -> items);
+    free(vec);
+}
