98968a564e9868b6b6a02a143470a91a5d1bd81b
ApChris <achris.info@gmail.com>
2020-06-05 04:17:23 +0300

    I have to add threads

--- WhoServer/src/server.c
+++ WhoServer/src/server.c
@@ -1,14 +1,45 @@
-
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
 #include "../include/myVector.h"
 
+// arguments
 long queryPortNum;
 long statisticsPortNum;
 long numThreads;
 long bufferSize;
 
+// threads
+pthread_mutex_t mutex;
+pthread_cond_t condinationVariable;
+
+// MyVecors
+myVector * threads = NULL;
+myVector * buffer = NULL;
+
+// sockets
+long sock;
+long newSock;
+long optval = 1;
 
 int main(int argc, char const *argv[])
 {
+
+    struct sockaddr_in server;
+    long serverLength;
+    struct sockaddr * serverPointer;
+
+    struct sockaddr_in client;
+    struct sockaddr * clientPointer;
+    long clientLength;
+
+
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&condinationVariable, NULL);
+
     if (argc != 9)                          // Check if we have !=9 arguments
     {
       printf("ERROR INPUT!!\nGive for example : ./whoServer -q 100 -s 200 -w 5 -b 100\n");
@@ -20,37 +51,78 @@ int main(int argc, char const *argv[])
       if(!strcmp(argv[i],"-q"))         // Get queryPortNum
       {
           queryPortNum = atol(argv[i+1]);
+          if(queryPortNum <= 0)
+          {
+              printf("Invalid query port number!\n");
+              exit(EXIT_FAILURE);
+          }
       }
       else if(!strcmp(argv[i],"-s"))    // Get statisticsPortNum
       {
          statisticsPortNum = atol(argv[i+1]);
+         if(statisticsPortNum <= 0)
+         {
+             printf("Invalid statistics port number!\n");
+             exit(EXIT_FAILURE);
+         }
       }
       else if(!strcmp(argv[i],"-w"))    // Get numThreads
       {
          numThreads = atol(argv[i+1]);
+          if(numThreads <= 0)
+         {
+             printf("Invalid threads number!\n");
+             exit(EXIT_FAILURE);
+         }
       }
       else if(!strcmp(argv[i],"-b"))    // Get buffer Size
       {
          bufferSize = atol(argv[i+1]);
+         if(bufferSize <= 0)
+         {
+             perror("Invalid buffer size!");
+             exit(EXIT_FAILURE);
+         }
       }
 
     }
 
-    myVector * vector = NULL;
-    vector = Init_MyVector(vector);;
-
-
-    PushBack_MyVector(vector, "Hello");
-
-
-    printf("%ld\n",vector -> size);
-    for (long i = 0; i < 20; i++)
+    // Create sockets
+    if((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1)
     {
-        // printf("Hello\n");
-        UpdateItem_MyVector(vector,"Bye",i);
-        // printf("%s\n", (char *)vector -> items[i]);
+        perror("Socket has been failed:");
+        exit(EXIT_FAILURE);
     }
 
-    Deallocate_MyVector(vector);
+    // Helps in manipulating options, prevents error such as address already in use
+    if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(long)) < 0)
+    {
+        perror("Setsockopt has been failed:");
+        exit(EXIT_FAILURE);
+    }
+
+
+    // set server
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(queryPortNum);
+    serverPointer = (struct sockaddr *)&server;
+    serverLength = sizeof(server);
+
+    if(bind(sock, serverPointer, serverLength) < 0)
+    {
+        perror("Bind has been failed:");
+        exit(EXIT_FAILURE);
+    }
+
+    if(listen(sock,5) < 0)
+    {
+        perror("Listen has been failed:");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server with pid = %ld\n",(long)getpid());
+
+
     return 0;
 }
