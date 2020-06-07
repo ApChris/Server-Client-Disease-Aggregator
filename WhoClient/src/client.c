b4bac362a69a7c703fbbfec12c6edbeb59ab843b
ApChris <achris.info@gmail.com>
2020-06-07 22:28:15 +0300

    client.c version

--- WhoClient/src/client.c
+++ WhoClient/src/client.c
@@ -9,7 +9,10 @@
 #include <netinet/in.h>
 #include "../../WhoServer/include/myVector.h"
 #include "../../Master/include/list.h"
+
 #define MAXIMUMLINE 4096
+
+#define MAXIMUMBUFFER 4096
 
 // arguments
 char * queryFile;
@@ -32,10 +35,14 @@ PathNode * queries = NULL;
 long sock;
 long newSock;
 long optval = 1;
+long bufferSize = 1;
 
+long ReadFromSocket(long fileDescriptor, char * buffer);
+// void WriteToSocket(long fileDescriptor, char * buffer);
 
-void * queriesServer();
-
+// long ReadFromSocket(long fileDescriptor, char * message);
+void WriteToSocket(long fileDescriptor, char * message);
+void * mainThreadServer(void * argvp);
 void ReadFile(const char * queryFile)
 {
     char * line = NULL;
@@ -73,7 +80,7 @@ void ReadFile(const char * queryFile)
 int main(int argc, char const *argv[])
 {
     pthread_t * thread;
-    char message[MAXBUFFER];
+    char message[MAXBUFFER] = "";
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&condinationVariable, NULL);
 
@@ -135,13 +142,186 @@ int main(int argc, char const *argv[])
     }
     else
     {
-        // pthread_create(thread, NULL, queriesServer ,NULL);
+        pthread_create(thread, NULL, mainThreadServer ,NULL);
         PushBack_MyVector(threadsVector,thread);
     }
 
-
+    pthread_cond_signal(&condinationVariable);
+    // 
+    // for (long i = 0; i < 1; i++) {
+    //     pthread_join(GetItem_MyVector(threadsVector,i), NULL);
+    // }
     // TESTING PART
 
+    // struct sockaddr_in server;
+    // uint64_t serverLength;
+    // struct sockaddr * serverPointer;
+    //
+    // struct sockaddr_in client;
+    // struct sockaddr * clientPointer;
+    // uint64_t clientLength;
+    //
+    // // For gethostbyname
+    // struct hostent * gtHName;
+    //
+    // // Create sockets
+    // if((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+    // {
+    //     perror("Client: Socket has been failed:");
+    //     exit(EXIT_FAILURE);
+    // }
+    //
+    // // Helps in manipulating options, prevents error such as address already in use
+    // if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(long)) < 0)
+    // {
+    //     perror("Client: Setsockopt has been failed:");
+    //     exit(EXIT_FAILURE);
+    // }
+    //
+    // if((gtHName = gethostbyname(servIP)) == NULL)
+    // {
+    //     perror("Client: gethostbyname has been failed:");
+    //     exit(EXIT_FAILURE);
+    // }
+    //
+    // // set server
+    // server.sin_family = AF_INET;
+    // bcopy((char *) gtHName -> h_addr, (char *) &server.sin_addr, gtHName -> h_length);
+    //
+    // // server.sin_addr.s_addr = htonl(INADDR_ANY);
+    // server.sin_port = htons(servPort);
+    // serverPointer = (struct sockaddr *)&server;
+    // serverLength = sizeof(server);
+    //
+    //
+    // if(connect(sock, serverPointer, serverLength) == -1)
+    // {
+    //     perror("Client: connect has been failed:");
+    //     exit(EXIT_FAILURE);
+    // }
+    // printf("edw\n");
+    // WriteToSocket(sock, "Hello fRom Client");
+    // // printf("Client with pid: %ld -> %s\n",(long)getpid(),message);
+    //
+    // WriteToSocket(sock, "Bye Bye");
+    // // printf("Client with pid: %ld -> %s\n",(long)getpid(),message);
+    //
+    // printf("Client with pid = %ld is listening on port:%ld\n",(long)getpid(), servPort);
+    //
+    //
+    //
+
+
+    // END OF TESTING PART
+
+
+    PrintList_Path(&queries);
+    DeleteList_Path(&queries);
+    free(queryFile);
+    free(servIP);
+
+    return 0;
+}
+
+
+
+
+//
+long ReadFromSocket(long fileDescriptor, char * buffer)
+{
+    long bytesNumber;
+    long length = 0;
+    long counter = 0;
+
+    char finalBuffer[MAXIMUMBUFFER] = "";
+    char tempBuffer[MAXIMUMBUFFER] = "";
+
+    if(read(fileDescriptor, &length, sizeof(length)) > 0)
+    {
+
+
+        long quotient = length/bufferSize;
+        if(quotient == 0)
+        {
+            if( (bytesNumber = read(fileDescriptor,tempBuffer, length)) >= 0)
+            {
+                char chunkLength[length];
+                strcpy(chunkLength,tempBuffer);
+                strcat(finalBuffer,chunkLength);
+            }
+
+        }
+        else
+        {
+            // for every chunk
+            for (long i = 0; i < quotient; i++)
+            {
+                char chunk[bufferSize];
+
+                // read bufferSize bytes and append them to finalBuffer
+                if( (bytesNumber = read(fileDescriptor,tempBuffer, bufferSize)) >= 0)
+                {
+                    strcpy(chunk,tempBuffer);
+
+                    strcat(finalBuffer,chunk);
+                    // for last bytes
+                    if(i + 1 == quotient)
+                    {
+                        char lastChunk[bufferSize];
+                        long remainder = length - (quotient*bufferSize);
+                        bytesNumber = read(fileDescriptor, tempBuffer, remainder);
+
+                        strcpy(lastChunk,tempBuffer);
+                        strcat(finalBuffer,lastChunk);
+                    }
+                }
+            }
+        }
+        counter += length;
+
+    }
+    strcpy(buffer, finalBuffer);
+    return counter;
+}
+
+void WriteToSocket(long fileDescriptor, char * buffer)
+{
+    long length = strlen(buffer) + 1;
+
+    if(write(fileDescriptor, &length, sizeof(length)) < 0)
+    {
+        perror("Client:WriteToSocket has been failed:");
+        exit(EXIT_FAILURE);
+    }
+
+    // if bufferSize > length . just read length bytes
+    long quotient = length/bufferSize;
+    if(quotient == 0)
+    {
+        write(fileDescriptor,buffer, length);
+    }
+    // else split message in chunks
+    else
+    {
+        for (long i = 0; i < quotient; i++)
+        {
+            write(fileDescriptor,buffer + bufferSize*i, bufferSize);
+            if(i + 1 == quotient)
+            {
+                long remainder = length - (quotient*bufferSize);
+                write(fileDescriptor, buffer + bufferSize*(i+1), remainder);
+            }
+        }
+    }
+
+}
+
+
+
+
+//
+void * mainThreadServer(void * argp)
+{
     struct sockaddr_in server;
     uint64_t serverLength;
     struct sockaddr * serverPointer;
@@ -188,103 +368,16 @@ int main(int argc, char const *argv[])
         perror("Client: connect has been failed:");
         exit(EXIT_FAILURE);
     }
+    printf("edw\n");
+    WriteToSocket(sock, "Hello fRom Client");
+    // printf("Client with pid: %ld -> %s\n",(long)getpid(),message);
 
-    // ReadFromSocket(sock, );
-
-
-    // if(bind(sock, serverPointer, serverLength) < 0)
-    // {
-    //     perror("Bind has been failed:");
-    //     exit(EXIT_FAILURE);
-    // }
-    //
-    // if(listen(sock,5) < 0)
-    // {
-    //     perror("Listen has been failed:");
-    //     exit(EXIT_FAILURE);
-    // }
+    WriteToSocket(sock, "Bye Bye");
+    // printf("Client with pid: %ld -> %s\n",(long)getpid(),message);
 
     printf("Client with pid = %ld is listening on port:%ld\n",(long)getpid(), servPort);
 
 
 
 
-
-    // END OF TESTING PART
-
-
-    PrintList_Path(&queries);
-    DeleteList_Path(&queries);
-    free(queryFile);
-    free(servIP);
-
-    return 0;
-}
-
-long ReadFromSocket(long fileDescriptor, char * message)
-{
-    char character;
-    long lengthOfMessage;
-    long i;
-    if(read(fileDescriptor,&character, 1) < 0)
-    {
-        perror("Client:ReadFromSocket has been failed");
-        exit(EXIT_FAILURE);
-    }
-
-    lengthOfMessage = character;
-
-    for (i = 0; i < lengthOfMessage; i++)
-    {
-        i += read(fileDescriptor, &message[i], lengthOfMessage - i);
-    }
-    return i;
-}
-
-void * queriesServer()
-{
-    struct sockaddr_in server;
-    uint64_t serverLength;
-    struct sockaddr * serverPointer;
-
-    struct sockaddr_in client;
-    struct sockaddr * clientPointer;
-    uint64_t clientLength;
-
-
-    // Create sockets
-    if((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1)
-    {
-        perror("Client: Socket has been failed:");
-        exit(EXIT_FAILURE);
-    }
-
-    // Helps in manipulating options, prevents error such as address already in use
-    if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(long)) < 0)
-    {
-        perror("Client: Setsockopt has been failed:");
-        exit(EXIT_FAILURE);
-    }
-
-
-    // set server
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port = htons(servPort);
-    serverPointer = (struct sockaddr *)&server;
-    serverLength = sizeof(server);
-
-    if(bind(sock, serverPointer, serverLength) < 0)
-    {
-        perror("Bind has been failed:");
-        exit(EXIT_FAILURE);
-    }
-
-    if(listen(sock,5) < 0)
-    {
-        perror("Listen has been failed:");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Client with pid = %ld is listening on port:%ld\n",(long)getpid(), servPort);
 }
